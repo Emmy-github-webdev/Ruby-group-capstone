9ee86c9380f4d4cd3c01d2347a3371facfadaf9f
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-15 11:11:45 +0100

    Add create musicalbum method

--- logic/logic_app.rb
+++ logic/logic_app.rb
@@ -1,1 +1,20 @@
-# Add all the logic operations here.
+class Logic
+  def initialize(add_musicalbum = {})
+    @add_musicalbum = add_musicalbum
+    @musicalbum_array = []
+  end
+  
+  def add_new_musicalbum
+    @add_musicalbum.musicalbums.each { |musicalbum| 
+      @musicalbum_array << {
+        'name' => musicalbum.name,
+        'on_spotify' => musicalbum.on_spotify,
+        'publish_date' => musicalbum.publish_date,
+        'genre' => musicalbum.genre,
+        'author' => musicalbum.author,
+        'label' => musicalbum.label
+
+      }
+    }
+  end 
+end
