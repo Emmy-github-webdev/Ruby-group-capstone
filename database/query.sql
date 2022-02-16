c066ae04743354eeecbef5ddc4e247fbf4214079
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-16 14:30:49 +0100

    Create genre table

--- database/query.sql
+++ database/query.sql
@@ -1,8 +1,14 @@
  
-CREATE TABLE MusicAlbum(
+CREATE TABLE musicabums(
   id int GENERATED ALWAYS AS IDENTITY,
   on_spotify boolean,
   PRIMARY KEY(id),
   item_id int NOT NULL,
   CONSTRAINT kf_items FOREIGN KEY(item_id) REFERENCES items(id) ON DELETE CASCADE
+);
+
+CREATE TABLE genres (
+  id int,
+  PRIMARY KEY(id),
+  name varchar(45)
 );
