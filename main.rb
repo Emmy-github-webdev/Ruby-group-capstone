075d9a2b6a32dde8af3ae275cd9c903693013c0e
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-16 11:01:33 +0100

    Update main class

--- main.rb
+++ main.rb
@@ -1,14 +1,8 @@
-Dir['./classes/*.rb'].sort.each { |file| require file}
-require_relative 'logic/app'
+require_relative 'app'
 
 def main
-  optionselected = DisplayMenuOption.new
-  loop do
-    optionselected.menu
-    user_choice = gets.chomp.to_i
-    optionselected.option(user_choice)
-    break if user_choice == 4
-  end
+  app = App.new
+  app.run
 end
 
 main
