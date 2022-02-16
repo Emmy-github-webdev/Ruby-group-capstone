ac1bf479aa875cf8b3e2c442e9990a7cc943d16e
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-16 14:31:52 +0100

    Create item table

--- database/query.sql
+++ database/query.sql
@@ -1,5 +1,5 @@
  
-CREATE TABLE musicabums(
+CREATE TABLE musicalbum(
   id int GENERATED ALWAYS AS IDENTITY,
   on_spotify boolean,
   PRIMARY KEY(id),
@@ -12,3 +12,17 @@ CREATE TABLE genres (
   PRIMARY KEY(id),
   name varchar(45)
 );
+
+CREATE TABLE items (
+  id int GENERATED ALWAYS AS IDENTITY NOT NULL,
+  PRIMARY KEY (id),
+  genre varchar(30),
+  author varchar(30),
+  label varchar(30),
+  publish_date int,
+  archived boolean,
+  album_id int, 
+  genre_id int,
+  CONSTRAINT kf_albums FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE,
+  CONSTRAINT kf_genres FOREIGN KEY(genre_id) REFERENCES genres(id) ON DELETE CASCADE
+); 
