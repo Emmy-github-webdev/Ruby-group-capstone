a758dc4679c05e7b7644ffdc199ceb028f0dfe7a
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-16 13:52:25 +0100

    Add can be archived test for music album

--- spec/musicalbum_spec.rb
+++ spec/musicalbum_spec.rb
@@ -1,16 +1,21 @@
 require 'rspec'
 require_relative '../classes/musicalbum'
-require_relative '../classes/genre'
 
 describe MusicAlbum do
-  before :each do
-    @musicalbum = MusicAlbum.new('02/02/2020', 'Emmanuel Ogah', 'True', 'Drama')
+  before(:each) do
+    @musicalbum = MusicAlbum.new('2020', 'Emmanuel', 'true', 'Drama')
   end
+  context 'Testing music album class' do
+    describe 'Check the properties of MusicAlbum class' do
+      it 'Returns the properties of Music Album without error' do
+        expect(@musicalbum).to be_instance_of(MusicAlbum)
+      end
 
-  describe 'Check the properties of MusicAlbum class' do
-    it 'Returns the properties of Music Album without error' do
-      expect(@musicalbum).to be_instance_of(MusicAlbum)
+      it 'It can be archived?' do
+        expect(@musicalbum.archived).to be false
+      end
     end
+
   end
   
 end
