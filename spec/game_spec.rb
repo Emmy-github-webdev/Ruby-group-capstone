15ec2c1c70901adf827e62c17846b23dcf960472
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-18 20:32:37 +0100

    Fix the breaking rspec test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require_relative '../classes/game'
 require 'date'
 describe 'Game class should work without error' do
   before :each do
-    @game = Game.new('Jim', true, 2020, 2005)
+    @game = Game.new('Jim', 'spinner', true, 2020, 2005)
   end
   it 'instantiates a Game' do
     expect(@game).to be_instance_of(Game)
